a3e867655b07a4b4e8640de956861b99675999b9
Duxoo <32924675+Duxoo@users.noreply.github.com>
2018-01-25 18:07:02 +0300

    objects and arrays
    added simple functions for objects and arrays

--- main.js
+++ main.js
@@ -32,3 +32,64 @@ if(str.length > maxlength)
     }
   else alert(str);  
 }
+/*
+
+FUNCTIONS FOR OBJECTS
+
+*/
+
+//Is object empty?returns true if object is empty
+function isEmpty(obj) {
+    for(var key in obj) {    
+        return false;
+    }
+    return true;
+}
+
+//Sum of values of every name in object
+function salaries(obj) {
+    var result=0;
+    for(var key in obj)
+        result+=obj[key];
+    return result;
+}
+
+//Max value in object 
+function maxValue(obj) {
+    var max=0;
+    for(var key in obj) {
+        if(obj[key] > max) max = obj[key];
+    }
+     return max;   
+}
+
+/*
+
+FUNCTIONS FOR ARRAYS
+
+*/
+
+//random from min to max
+var array = [1,2,3,4,5,6];
+function random(min,max) {
+    return min + Math.floor(Math.random() * (max + 1 - min));
+}
+
+//returns random value from array
+function randomValueInArray(array)
+{
+    return array[random(0,array.length -1 )];
+}
+
+//returns last element from array
+function lastElement(arr) {
+    return arr[arr.length - 1];
+}
+
+//returns the index of value in array if value exists and 0 if not
+function findValueInArray(array, value) {
+    for(var i=0; i < array.length - 1 ; i++) {
+        if(array[i] === value) return i;
+    }
+    return -1;
+}
