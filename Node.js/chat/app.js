804d1c567b8590e9f94804c92ad712fe6b1d449f
Duxoo <32924675+Duxoo@users.noreply.github.com>
2018-06-11 23:17:54 +0300

    commit

--- Node.js/chat/app.js
+++ Node.js/chat/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 var mongoose = require('./libs/mongoose');
 //сессии
 var session = require('express-session');
@@ -23,6 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(bodyParser);
 //для того, чтобы сохранять и загружать сессии нужен класс connect-mongo,
 //к которому будет обращаться middleware session
 const MongoStore = require('connect-mongo')(session);
---
+++ Node.js/chat/routes/login.js
@@ -0,0 +1,56 @@
+var User = require('../models/user').User;
+var createError = require('http-errors');
+var async = require('async');
+var session = require('express-session');
+//.waterfall получает массив задач и выполняет их одна за другой
+exports.get = function(req, res) {
+	res.render('login');
+};
+
+exports.post = function(req, res, next) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	async.waterfall([
+		//сначала выпонляется эта функция
+		function(callback) {
+			User.findOne({username: username},callback);
+		},
+		//затем передастся сюда, если найдено - первый аргумент user, иначе null
+		function(user, callback) {
+			//есть ли такой пользователь
+			if(user) {
+				//проверка пароля
+				if(user.checkPassword(password)) {
+					//если все хорошо, передаем следующей функции
+					callback(null, user);
+				}
+				//иначе выдаем ошибку 403
+				else {
+					return next(createError(403, "Invalid password"))
+					next()
+				}
+			}
+			//если пользователя нет
+			else {
+				//создаем его
+				var user = new User({username: username, password: password});
+				//записываем в базу
+				user.save(function(err) {
+					if(err) return next(err);
+					//передаем управление следующей функции
+					callback(null,user);
+				});
+			}
+		}
+
+		], function(err, user) {
+			//если пришла ошибка, возвращаем ее
+			if(err) return next(err);
+			//иначе записываем в сесси id пользователя
+			else {
+				session.user = user.id;
+				res.send({});
+			}
+		});
+}
